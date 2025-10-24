764b514ee827c7c2d140a0e6928424f400613fa5
maryam <maryam11598@bazeuniversity.edu.ng>
2025-10-24 20:18:30 +0100

    init

--- Start/Overview/DataTypes/Program.cs
+++ Start/Overview/DataTypes/Program.cs
@@ -12,10 +12,28 @@ bool tf = default;
 Console.WriteLine($"{a}, {b}, {tf}, {c}, {f}, {d}");
 
 // TODO: implicit type conversion
-
+onsole.WriteLine($"{c + a}"); 
+Console.WriteLine($"{(char)(c + a)}");
+Console.WriteLine($"{f + a}");
+ Console.WriteLine($"{f + c}");
 
 // TODO: Create an instance of a struct (which is a value type)
+ss1;
+s1.a = 5;
+s1.b = false;
 
+ // Perform an operation on a struct
+void StructOp(s theStruct) {
+// Modify the struct properties inside the function
+theStruct.a = 10;
+ theStruct.b = true;
+Console.WriteLine($"{theStruct.a}, {theStruct.b}"); }
+
+ Console.WriteLine("Structs are passed by copy, since they are value
+types:");
+Console.WriteLine($"{s1.a}, {s1.b}");
+ StructOp(s1);
+Console.WriteLine($"{s1.a}, {s1.b}"); 
 
 // Perform an operation on a struct
 void StructOp(s theStruct) {
@@ -25,12 +43,13 @@ void StructOp(s theStruct) {
     Console.WriteLine($"{theStruct.a}, {theStruct.b}");
 }
 
-// Console.WriteLine("Structs are passed by copy, since they are value types:");
-// Console.WriteLine($"{s1.a}, {s1.b}");
-// StructOp(s1);
-// Console.WriteLine($"{s1.a}, {s1.b}");
+Console.WriteLine("Structs are passed by copy, since they are value types:");
+Console.WriteLine($"{s1.a}, {s1.b}");
+StructOp(s1);
+Console.WriteLine($"{s1.a}, {s1.b}");
 
 // TODO: Create an object instance of a class (which is a reference type)
+MyClass cl = new MyClass{a = 5, b = false};
 
 
 // Perform an operation on the class
@@ -41,10 +60,10 @@ void ClassOp(MyClass theClass) {
     Console.WriteLine($"{theClass.a}, {theClass.b}");
 }
 
-// Console.WriteLine("Objects are passed by reference, since they are reference types:");
-// Console.WriteLine($"{cl.a}, {cl.b}");
-// ClassOp(cl);
-// Console.WriteLine($"{cl.a}, {cl.b}");
+Console.WriteLine("Objects are passed by reference, since they are reference types:");
+Console.WriteLine($"{cl.a}, {cl.b}");
+ClassOp(cl);
+Console.WriteLine($"{cl.a}, {cl.b}");
 
 // These are declared at the bottom of the file because C# requires
 // top-level statements to come before type declarations
