acbc7ec7c02075ad332ffc9f86fd9661b0ed4e93
maryam <maryam11598@bazeuniversity.edu.ng>
2025-10-24 20:41:58 +0100

    INIT

--- Start/Strings/Interpolation/Program.cs
+++ Start/Strings/Interpolation/Program.cs
@@ -11,10 +11,13 @@ string c = "CSharp";
 Console.WriteLine("The values are {0}, {1} and {2}", a, b, c);
 
 // TODO: Using string interpolation, the code is much easier to read
-
+Console.WriteLine("{0,-15} {1,10}", "Float Val", "Int Val"); 
+Console.WriteLine("{0,-15} {1,10}", f1, i1);
 
 // TODO: Interpolated strings can contain expressions as well
-
+Console.WriteLine("{0,-15} {1,10}", "Float Val", "Int Val"); 
+Console.WriteLine("{0,-15} {1,10}", f1, i1);
 
 // TODO: Complex objects can be embedded in strings this way as well
-
+Console.WriteLine("{0,-15} {1,10}", "Float Val", "Int Val"); 
+Console.WriteLine("{0,-15} {1,10}", f1, i1);
